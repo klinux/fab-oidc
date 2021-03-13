c2101257d0c531bfef903f423eecb7a7bab00db0
Kleber Rocha <klinux@gmail.com>
2021-03-13 02:36:28 -0300

    Feature | add support to airflow 2

--- fab_oidc/views.py
+++ fab_oidc/views.py
@@ -1,4 +1,6 @@
 import os
+import logging
+
 from flask import redirect, request
 from flask_appbuilder.security.views import AuthOIDView
 from flask_login import login_user
@@ -6,12 +8,40 @@ from flask_admin import expose
 from urllib.parse import quote
 
 
-# Set the OIDC field that should be used as a username
-USERNAME_OIDC_FIELD = os.getenv('USERNAME_OIDC_FIELD', default='sub')
-FIRST_NAME_OIDC_FIELD = os.getenv('FIRST_NAME_OIDC_FIELD',
-                                  default='nickname')
-LAST_NAME_OIDC_FIELD = os.getenv('LAST_NAME_OIDC_FIELD',
-                                 default='name')
+USERNAME_OIDC_FIELD = os.getenv(
+    'USERNAME_OIDC_FIELD',
+    default='preferred_username',
+)
+
+FIRST_NAME_OIDC_FIELD = os.getenv(
+    'FIRST_NAME_OIDC_FIELD',
+    default='given_name',
+)
+
+LAST_NAME_OIDC_FIELD = os.getenv(
+    'LAST_NAME_OIDC_FIELD',
+    default='family_name',
+)
+
+EMAIL_OIDC_FIELD = os.getenv(
+    'EMAIL_OIDC_FIELD',
+    default='email',
+)
+
+KEYCLOAK_CLIENT_ROLE_OIDC_FIELD = os.getenv(
+    'KEYCLOAK_CLIENT_ROLE_OIDC_FIELD',
+    default='roles',
+)
+
+KEYCLOAK_RESOURCE_ACCESS_APP_OIDC_FIELD = os.getenv(
+    'KEYCLOAK_RESOURCE_ACCESS_APP_OIDC_FIELD',
+    default='superset',
+)
+
+OIDC_LOGOUT_URI = 'OIDC_LOGOUT_URI'
+
+
+logger = logging.getLogger(__name__)
 
 
 class AuthOIDCView(AuthOIDView):
@@ -24,23 +54,39 @@ class AuthOIDCView(AuthOIDView):
 
         @self.appbuilder.sm.oid.require_login
         def handle_login():
-            user = sm.auth_user_oid(oidc.user_getfield('email'))
+            user = sm.auth_user_oid(oidc.user_getfield(EMAIL_OIDC_FIELD))
+            info = oidc.user_getinfo([
+                USERNAME_OIDC_FIELD,
+                FIRST_NAME_OIDC_FIELD,
+                LAST_NAME_OIDC_FIELD,
+                EMAIL_OIDC_FIELD,
+                KEYCLOAK_CLIENT_ROLE_OIDC_FIELD,
+            ])
 
             if user is None:
-                info = oidc.user_getinfo([
-                    USERNAME_OIDC_FIELD,
-                    FIRST_NAME_OIDC_FIELD,
-                    LAST_NAME_OIDC_FIELD,
-                    'email',
-                ])
-
                 user = sm.add_user(
                     username=info.get(USERNAME_OIDC_FIELD),
                     first_name=info.get(FIRST_NAME_OIDC_FIELD),
                     last_name=info.get(LAST_NAME_OIDC_FIELD),
-                    email=info.get('email'),
+                    email=info.get(EMAIL_OIDC_FIELD),
                     role=sm.find_role(sm.auth_user_registration_role)
                 )
+                logger.info(f"user added: {info.get(EMAIL_OIDC_FIELD)}")
+
+            # sync roles from keycloak to flask
+            user.roles.clear()
+            sm.update_user(user)
+
+            if info.get(KEYCLOAK_CLIENT_ROLE_OIDC_FIELD) is None:
+                logger.error(
+                    f'user {info.get(EMAIL_OIDC_FIELD)} does not have ROLE')
+                logger.error(f'user info: {info}')
+            else:
+                for role in info.get(KEYCLOAK_CLIENT_ROLE_OIDC_FIELD):
+                    user.roles.append(sm.find_role(role))
+                    logger.info(
+                        f"assign role: {role}, find_role: {sm.find_role(role)} to user: {info.get(EMAIL_OIDC_FIELD)}")
+                sm.update_user(user)
 
             login_user(user, remember=False)
             return redirect(self.appbuilder.get_url_for_index)
@@ -51,14 +97,13 @@ class AuthOIDCView(AuthOIDView):
     def logout(self):
 
         oidc = self.appbuilder.sm.oid
-
         oidc.logout()
         super(AuthOIDCView, self).logout()
-        redirect_url = request.url_root.strip(
-            '/') + self.appbuilder.get_url_for_login
+        
+        redirect_url = request.url_root.strip('/') + self.appbuilder.get_url_for_login
 
-        logout_uri = oidc.client_secrets.get(
-            'issuer') + '/protocol/openid-connect/logout?redirect_uri='
+        logout_uri = oidc.client_secrets.get('issuer') + '/protocol/openid-connect/logout?redirect_uri='
+
         if 'OIDC_LOGOUT_URI' in self.appbuilder.app.config:
             logout_uri = self.appbuilder.app.config['OIDC_LOGOUT_URI']
 
